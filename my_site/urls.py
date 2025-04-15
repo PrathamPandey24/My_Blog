cfe5b53ed529d0f7cc5e8c44e15e573047081bbc
PrathamPandey24 <prthmpnd24@gmail.com>
2025-04-15 14:32:12 +0530

    .

--- my_site/urls.py
+++ my_site/urls.py
@@ -22,7 +22,7 @@ from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
+    path('', include('blog.urls')),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)\
     + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 
