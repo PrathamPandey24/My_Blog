0f9d945d30b0d6814742a8dc533ec3f526cd20cd
PrathamPandey24 <prthmpnd24@gmail.com>
2025-04-15 14:30:18 +0530

    Add Comment model, form, and views; refactor URL patterns and admin registration

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
-from .models import Post, Tag, Author
+from .models import Post, Tag, Author, Comment
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
     list_filter=("author","tags","date",)
     list_display=("title","date","author",)
-prepopulated_fields={"alug":("title")}
+    prepopulated_fields={"slug":("title",)}
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display=("user_name","post")
+
+
+
+
+
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Tag)
 admin.site.register(Author)
+admin.site.register(Comment, CommentAdmin)
+
--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,8 @@ from . import views
 
 
 urlpatterns=[
-    path("", views.starting_page, name="starting-page"),                                            
-    path("posts",views.posts , name="posts-page"),
-    path("posts/<slug:slug>",views.post_details, name="post-detail-page"),
-]
+    path("", views.StartingPageView.as_view(), name="starting-page"),                                            
+    path("posts",views.AllPostsView.as_view() , name="posts-page"),
+    path("posts/<slug:slug>/",views.SinglePostView.as_view(), name="post-detail-page"),
+    path("read-later",views.ReadLaterView.as_view(),name="read-later"),
+ ] 
